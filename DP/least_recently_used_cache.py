ec7ff2c1b9728ac2166900640893fbb7cf8bfe19
vkd225 <vkd225@nyu.edu>
2022-07-13 02:15:26 -0400

    lru caches

--- DP/least_recently_used_cache.py
+++ DP/least_recently_used_cache.py
@@ -1,58 +1,112 @@
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.dq = collections.deque()
+        self.valueMap = {}
 
-from collections import OrderedDict
+    def get(self, key: int) -> int:
+        if key not in self.valueMap:
+            return -1
+        self.dq.remove(key)
+        self.dq.append(key)
+
+        return self.valueMap[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key not in self.valueMap:
+            if len(self.dq) == self.cap:
+                del self.valueMap[self.dq.popleft()]
+        else:
+            self.dq.remove(key)
+
+        self.dq.append(key)
+        self.valueMap[key] = value
+
+
+
+#  using doubly linkedlist
+
+class LinkedNode(object):
+
+    def __init__(self, val, key):
+        self.val = val     # 用来存放真正的缓存值
+        self.key = key     # 反向指向字典中的一个key，用来进行反向删除操作
+        self.next = None
+        self.prev = None
+
 
 class LRUCache:
 
-    # initialising capacity
+
     def __init__(self, capacity: int):
-        self.cache = OrderedDict()
         self.capacity = capacity
+        self.lookup = {}
+        self.dummy = LinkedNode(0,0)
+        self.head = self.dummy.next
+        self.tail = self.dummy.next
 
-    # we return the value of the key
-    # that is queried in O(1) and return -1 if we
-    # don't find the key in out dict / cache.
-    # And also move the key to the end
-    # to show that it was recently used.
-    def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
+    def remove_head_node(self):
+        if not self.head:
+            return
+        prev = self.head
+        self.head = self.head.next
+        if self.head:
+            self.head.prev = None
+        del prev
+
+    def append_new_node(self, new_node):
+        """  add the new node to the tail end
+        """
+        if not self.tail:
+            self.head = self.tail = new_node
         else:
-            self.cache.move_to_end(key)
-            return self.cache[key]
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = self.tail.next
 
-    # first, we add / update the key by conventional methods.
-    # And also move the key to the end to show that it was recently used.
-    # But here we will also check whether the length of our
-    # ordered dictionary has exceeded our capacity,
-    # If so we remove the first key (least recently used)
+
+    def unlink_cur_node(self, node):
+        """ unlink current linked node
+        """
+        if self.head is node:
+            self.head = node.next
+            if node.next:
+                node.next.prev = None
+            return
+
+        # removing the node from somewhere in the middle; update pointers
+        prev, nex = node.prev, node.next
+        prev.next = nex
+        nex.prev = prev
+
+
+
+    def get(self, key: int) -> int:
+        if key not in self.lookup:
+            return -1
+
+        node = self.lookup[key]
+
+        if node is not self.tail:
+            self.unlink_cur_node(node)
+            self.append_new_node(node)
+
+        return node.val
+
+
     def put(self, key: int, value: int) -> None:
-        self.cache[key] = value
-        self.cache.move_to_end(key)
-        if len(self.cache) > self.capacity:
-            self.cache.popitem(last = False)
+        if key in self.lookup:
+            self.lookup[key].val = value
+            self.get(key)
+            return
 
 
-# RUNNER
-# initializing our cache with the capacity of 2
-cache = LRUCache(2)
+        if len(self.lookup) == self.capacity:
+            # remove head node and correspond key
+            self.lookup.pop(self.head.key)
+            self.remove_head_node()
 
-
-
-cache.put(1, 1)
-print(cache.cache)
-cache.put(2, 2)
-print(cache.cache)
-cache.get(1)
-print(cache.cache)
-cache.put(3, 3)
-print(cache.cache)
-cache.get(2)
-print(cache.cache)
-cache.put(4, 4)
-print(cache.cache)
-cache.get(1)
-print(cache.cache)
-cache.get(3)
-print(cache.cache)
-cache.get(4)
-print(cache.cache)
+        # add new node and hash key
+        new_node = LinkedNode(val=value, key=key)
+        self.lookup[key] = new_node
+        self.append_new_node(new_node)
